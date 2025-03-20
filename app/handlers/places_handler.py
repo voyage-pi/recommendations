eaa3be13aa806fc4b205d78dda9f2f92246b1fcf
HenriqueFreitas07 <henriquefreitasm87@gmail.com>
2025-03-20 13:11:58 +0000

    request to places-wrapper

--- app/handlers/places_handler.py
+++ app/handlers/places_handler.py
@@ -1,7 +1,9 @@
 from typing import List, Tuple
 from app.schemas.Activities import PlaceInfo
 import requests as request
+import logging 
 
+logger = logging.getLogger('uvicorn.error')
 async def get_places_recommendations(
     latitude: float,
     longitude: float,
@@ -13,9 +15,11 @@ async def get_places_recommendations(
     Get place recommendations from Google Places API or another service
     This is a placeholder for your actual service wrapper implementation
     """
+    logger.debug("Entered the function")
+
     included_types=place_types[0]
     excluded_types=place_types[1]
-    url="localhost:"
+    url="http://host.docker.internal:8080/places"
     request_body={
         "location":{
             "latitude":latitude,
@@ -25,11 +29,34 @@ async def get_places_recommendations(
         "includedTypes":included_types,
         "excludedTypes":excluded_types,
     }
-
-    response= request.get(url,json=request_body)     
-    r=response.json()
-    places=r.get("places")
-    # not tested, idk if is able to make the cast
-    new_places=[PlaceInfo(**obj) for obj in places]
-
-    return new_places
+    logging.debug(f"Request body {request_body}")
+    response= request.post(url,json=request_body)     
+    logging.debug(f"response {response.json()}")
+    status = response.status_code
+    if status == 200:
+        responseBody=response.json().get("response");
+        places_google=responseBody.get("places")
+        logging.debug(f"Places:{places_google}")
+        places:List[PlaceInfo] =[]
+        for data in places_google:
+            place = PlaceInfo(
+                id=data.get("ID"),
+                name=data.get("name"),
+                location=data.get("location"),
+                types=data.get("types", []),
+                photos=data.get("photos", []),
+                accessibility_options=data.get("accessibilityOptions", {}),
+                opening_hours=data.get("OpeningHours", {}),
+                price_range=data.get("priceRange"),
+                rating=data.get("rating"),
+                international_phone_number=data.get("internationalPhoneNumber"),
+                national_phone_number=data.get("nationalPhoneNumber"),
+                allows_dogs=data.get("allowsDogs"),
+                good_for_children=data.get("goodForChildren"),
+                good_for_groups=data.get("goodForGroups"),
+            )
+            places.append(place)
+        return places
+    else:
+        logging.debug(f"Response status:{status}")
+        return []
